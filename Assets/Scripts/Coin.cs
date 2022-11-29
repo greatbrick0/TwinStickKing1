eaf655e3282036e6801b137fba0dc3b0fd29c9ae
Spencer <speharley@gmail.com>
2022-11-29 02:18:18 -0500

    Fixed merge conflicts

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -6,12 +6,9 @@ public class Coin : MonoBehaviour
 {
     public ManagerScript mgRef;
     GameObject player;
-<<<<<<< HEAD
 
     public int value = 10;
 
-=======
->>>>>>> main
     void Start()
     {
         mgRef = GameObject.Find("Manager").GetComponent<ManagerScript>();
@@ -21,23 +18,9 @@ public class Coin : MonoBehaviour
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-<<<<<<< HEAD
         if(collision.gameObject.GetComponent<PlayerScript>() != null)
         {
             mgRef.coinAmount += value;
-=======
-        if(collision.gameObject == player)
-        {
-            player = collision.gameObject;
-            if (this.gameObject.name == "10Credit")
-            {
-                mgRef.coinAmount += 10;
-            }
-            if (this.gameObject.name == "50Credit")
-            {
-                mgRef.coinAmount += 50;
-            }
->>>>>>> main
             Destroy(this.gameObject);
         }
     }
